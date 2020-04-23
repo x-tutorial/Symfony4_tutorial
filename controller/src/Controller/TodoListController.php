404874a843d1d7c6bf995875e092d8ebbcfc1c43
x-tutorial <64193624+x-tutorial@users.noreply.github.com>
2020-04-23 11:57:48 +0200

    Create TodoListController.php

---
+++ controller/src/Controller/TodoListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TodoListController extends AbstractController
+{
+    /**
+     * @Route("/", name="todo_list")
+     */
+    public function index()
+    {
+        return $this->render('todo_list/index.html.twig', [
+            'controller_name' => 'TodoListController',
+        ]);
+    }
+}
